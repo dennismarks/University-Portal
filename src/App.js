7b686b737d6a4f34cb7f0c13a4c52399f2d8aeac
Dennis M <dennismarkshome@gmail.com>
2019-10-21 08:10:00 -0600

    Put Header inside components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 /* Route components */
-import Header from "./pages/Header";
+import Header from "./components/Header";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Course from "./pages/Course";
+import User from "./pages/User";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           <Route path="/course" component={Course} />
+          <Route path="/user" component={User} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheets/index.css";
+import "./stylesheets/user.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
