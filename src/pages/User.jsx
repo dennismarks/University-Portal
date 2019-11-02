5a670e4d893d76eeda9dd5824e86b7298e2308e0
Dennis M <dennismarkshome@gmail.com>
2019-11-01 22:27:17 -0400

    Update User page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,101 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UsersContext } from "../context/UsersContext";
 import UserInfo from "../components/UserInfo";
 import CoursesCard from "../components/CoursesCard";
 
-class User extends React.Component {
-  state = {
-    userInfo: {
-      img: "avatar.png",
-      name: "Alex Liskov",
-      university: "University of Toronto",
-      program: "Computer Science, 2021"
-    },
-    courses: {
-      currentCourses: [
-        "CSC301 - Introduction to Software Engineering",
-        "CSC309 - Programming on the Web",
-        "CSC373 - Algorithm Design, Analysis & Complexity",
-        "CSC428 - Human-Computer Interaction",
-        "CSC236 - Introduction to the Theory of Computation",
-        "CSC236 - Introduction to the Theory of Computation "
-      ],
-      takenCourses: [
-        "CSC236 - Introduction to the Theory of Computation",
-        "CSC258 - Computer Organization"
-      ],
-      toTakeCourses: []
-    },
-    currentRender: 0,
-    takenRender: 0,
-    toTakeRender: 0
-  };
+const User = props => {
+  const usersContext = useContext(UsersContext);
+  const id = props.match.params.id;
+  const user = usersContext.users[id];
 
-  componentDidMount() {
-    const cur = this.state.courses.currentCourses.length;
-    const taken = this.state.courses.takenCourses.length;
-    const toTake = this.state.courses.toTakeCourses.length;
-
-    cur > 3
-      ? this.setState({ currentRender: 3 })
-      : this.setState({ currentRender: cur });
-
-    taken > 3
-      ? this.setState({ takenRender: 3 })
-      : this.setState({ takenRender: taken });
-
-    toTake > 3
-      ? this.setState({ toTakeRender: 3 })
-      : this.setState({ toTakeRender: toTake });
-  }
-
-  showMore = name => () => {
-    const cur = this.state.courses.currentCourses.length;
-    const taken = this.state.courses.takenCourses.length;
-    const toTake = this.state.courses.toTakeCourses.length;
-
-    if (name === "◦ Current Courses") {
-      this.state.currentRender === cur
-        ? this.setState({ currentRender: 3 })
-        : this.setState({ currentRender: cur });
-    } else if (name === "◦ Taken Courses") {
-      this.state.takenRender === taken
-        ? this.setState({ takenRender: 3 })
-        : this.setState({ takenRender: taken });
-    } else if (name === "◦ Future course") {
-      this.state.toTakeRender === toTake
-        ? this.setState({ toTakeRender: 3 })
-        : this.setState({ toTakeRender: toTake });
-    }
-  };
-
-  render() {
-    return (
-      <div>
-
-        <div className="main">
-          <UserInfo userInfo={this.state.userInfo}></UserInfo>
-          <CoursesCard
-            courses={this.state.courses.currentCourses}
-            toRender={this.state.currentRender}
-            showMore={this.showMore}
-            name="◦ Current Courses"
-          ></CoursesCard>
-          <CoursesCard
-            courses={this.state.courses.takenCourses}
-            toRender={this.state.takenRender}
-            showMore={this.showMore}
-            name="◦ Taken Courses"
-          ></CoursesCard>
-          <CoursesCard
-            courses={this.state.courses.toTakeCourses}
-            toRender={this.state.toTakeRender}
-            showMore={this.showMore}
-            name="◦ Future course"
-          ></CoursesCard>
-        </div>
+  return (
+    <div>
+      <div className="main">
+        <UserInfo userInfo={user.userInfo}></UserInfo>
+        <CoursesCard
+          courses={user.courses.currentCourses}
+          toRender={user.courses.currentCourses.length}
+          name="Current Courses"
+        ></CoursesCard>
+        <CoursesCard
+          courses={user.courses.takenCourses}
+          toRender={user.courses.takenCourses.length}
+          name="Taken Courses"
+        ></CoursesCard>
+        <CoursesCard
+          courses={user.courses.toTakeCourses}
+          toRender={user.courses.toTakeCourses.length}
+          name="Future course"
+        ></CoursesCard>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default User;
