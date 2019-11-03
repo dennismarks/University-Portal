3918afbf12b62e6f43f9ffa0b5636e273bf1784f
Dennis M <dennismarkshome@gmail.com>
2019-11-03 12:57:32 -0500

    Add link to a user's profile inside User Details

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import { UsersContext } from "../context/UsersContext";
+import { Link } from "react-router-dom";
 
 const UserDetails = () => {
   const { selectedUser, removeStudent } = useContext(UsersContext);
+  const MY_ROUTE = () => `/user/${selectedUser.userInfo.id}/`;
 
   return (
     <div className="user-details">
@@ -17,6 +19,15 @@ const UserDetails = () => {
           </p>
           <p>
             Id: <strong>{selectedUser.userInfo.id}</strong>
+          </p>
+          <p>
+            User's page:
+            <strong>
+              <Link className="user-link" to={MY_ROUTE}>
+                {" "}
+                link
+              </Link>
+            </strong>
           </p>
           <button onClick={() => removeStudent(selectedUser.userInfo.id)}>
             Remove
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,16 +4,14 @@ import clsx from "clsx";
 import "../stylesheets/coursesCard.css";
 import "../stylesheets/Admin.css";
 
-const UsersList = p => {
-  // const MY_ROUTE = userId => `/user/${userId}/`;
-
+const UsersList = () => {
   const usersContext = useContext(UsersContext);
   const { users, selectedUser, setSelectedUser } = usersContext;
 
   return (
     <div className="user-list">
       <h1>Users</h1>
-      {users.map(user => {
+      {users.slice(1).map(user => {
         // compose class name
         const userItemClassNames = clsx("user-item", {
           "user-item-selected": user.userInfo === selectedUser.userInfo
