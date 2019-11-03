008ceaa34fa69c0f2e017d9520b9795d1cdd6eba
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2019-11-02 23:56:06 -0400

    fix: make tags link to search page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,8 +41,8 @@ function Home() {
               courseName="Programming on the Web"
               description="An introduction to software development on the web. Concepts underlying the development of programs that operate on the web; survey of technological alternatives; greater depth on some technologies. Operational concepts of the internet and the web, static client content, dynamic client content, dynamically served content, n-tiered architectures, web development processes, and security on the web. Assignments involve increasingly more complex web-based programs. Guest lecturers from leading e-commerce firms will describe the architecture and operation of their web sites."
               tags={[
-                { name: "Arts & Sci.", link: "#" },
-                { name: "Computer Science", link: "#" }
+                { name: "Arts & Sci.", value: "Arts & Science" },
+                { name: "Computer Sci.", value: "Computer Science" }
               ]}
             />
             <CourseMiniCard
@@ -51,8 +51,8 @@ function Home() {
               courseName="Introduction to Software Engineering"
               description="An introduction to software development on the web. Concepts underlying the development of programs that operate on the web; survey of technological alternatives; greater depth on some technologies. Operational concepts of the internet and the web, static client content, dynamic client content, dynamically served content, n-tiered architectures, web development processes, and security on the web. Assignments involve increasingly more complex web-based programs. Guest lecturers from leading e-commerce firms will describe the architecture and operation of their web sites."
               tags={[
-                { name: "Arts & Sci.", link: "#" },
-                { name: "Computer Science", link: "#" }
+                { name: "Arts & Sci.", value: "Arts & Science" },
+                { name: "Computer Sci.", value: "Computer Science" }
               ]}
             />
           </div>
