e9ad3cd8e8dd54c0f142bce2cca93ff887425dfc
Arnav Verma <arnavverma@hotmail.com>
2019-11-02 21:15:25 -0400

    added comments to account login

--- src/components/CourseCommentsBox.js
+++ src/components/CourseCommentsBox.js
@@ -1,8 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import UserCommentBox from "./UserCommentBox";
+import AuthContext from "../context/AuthContext";
 
 function CourseCommentsBox(props) {
   const [count, setCount] = useState(5);
+  const [openComments, setOpenComments] = useState(false)
+  const {
+    auth: { isLoggedIn, role, userId }
+  } = useContext(AuthContext);
+  const commentData = [
+    {
+      user:"Arnav Verma",
+      rating: "5",
+      userId:"12",
+      comment: "Wooooooooooooow loved this course, best one ever!!!!!!"
+    },
+    {
+      user: "Timmy Tom",
+      rating:"3",
+      userId:"12",
+      comment:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum consequatur magni accusantium repellat quidem dolores fugiat doloremque expedita rerum tempora!"
+    },
+    {
+      user:"Arnav Verma",
+      rating: "5",
+      userId:"12",
+      comment: "Wooooooooooooow loved this course, best one ever!!!!!!"
+    },
+    {
+      user: "Timmy Tom",
+      rating:"3",
+      userId:"12",
+      comment:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum consequatur magni accusantium repellat quidem dolores fugiat doloremque expedita rerum tempora!"
+    },
+    {
+      user:"Arnav Verma",
+      rating: "5",
+      userId:"12",
+      comment: "Wooooooooooooow loved this course, best one ever!!!!!!"
+    },
+  ]
+
+  const userComments = [];
+  for (let i = 0; i < commentData.length; i++) {
+    const userComment = commentData[i];
+    userComments.push(
+      <UserCommentBox
+        key={userComment.userId}
+        user={userComment.user}
+        rating={userComment.rating}
+        userId={userComment.userId}
+        comment={userComment.comment}
+      />
+    );
+  }
 
   return (
     <div className="w-auto ml-10 mr-10 px-10 pb-10 my-5 bg-white shadow-md ">
@@ -10,47 +61,17 @@ function CourseCommentsBox(props) {
       <h4 className="mb-4 text-1xl font-medium">Average Rating: 4.5 / 5 </h4>
       <div className="overflow-auto h-64 pr-2">
         <div>
-          <UserCommentBox
-            user="Arnav Verma"
-            rating="5"
-            userId="12"
-            comment="Wooooooooooooow loved this course, best one ever!!!!!!"
-          />
-          <UserCommentBox
-            user="Timmy Tom"
-            rating="3"
-            userId="12"
-            comment=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum consequatur magni accusantium repellat quidem dolores fugiat doloremque expedita rerum tempora!
-"
-          />
-          <UserCommentBox
-            user="Billy Bong"
-            rating="5"
-            userId="12"
-            comment="onsectetur adipisicing elit. Ipsum consequatur magni accusantium repella"
-          />
-          <UserCommentBox
-            user="Arnav Verma"
-            rating="5"
-            userId="12"
-            comment="Wooooooooooooow loved this course, best one ever!!!!!!"
-          />
-          <UserCommentBox
-            user="Arnav Verma"
-            rating="5"
-            userId="12"
-            comment="Wooooooooooooow loved this course, best one ever!!!!!!"
-          />
-          <UserCommentBox
-            user="Arnav Verma"
-            rating="5"
-            userId="12"
-            comment="Wooooooooooooow loved this course, best one ever!!!!!!"
-          />
+          {userComments}
         </div>
       </div>
+      { isLoggedIn ? 
       <div className="flex justify-center">
-        <div onClick={() => { count <= 0 ? setCount(count) : setCount( count - 1)} } className="h-12 w-12 font-medium text-4xl rounded cursor-pointer active:bg-gray-200  bg-white shadow-md hover:bg-gray-100 pb-4 pt-0 px-5 mt-6">
+        <div
+          onClick={() => {
+            count <= 0 ? setCount(count) : setCount(count - 1);
+          }}
+          className="h-12 w-12 font-medium text-4xl rounded cursor-pointer active:bg-gray-200  bg-white shadow-md hover:bg-gray-100 pb-4 pt-0 px-5 mt-6"
+        >
           -
         </div>
         <div
@@ -59,7 +80,12 @@ function CourseCommentsBox(props) {
         >
           {count}
         </div>
-        <div onClick={() => { count >= 5 ? setCount(count) : setCount( count + 1)} } className="h-12 w-12 font-medium text-2xl rounded cursor-pointer bg-white shadow-md hover:bg-gray-100 py-2 px-4 mt-6 mr-6">
+        <div
+          onClick={() => {
+            count >= 5 ? setCount(count) : setCount(count + 1);
+          }}
+          className="h-12 w-12 font-medium text-2xl rounded cursor-pointer bg-white shadow-md hover:bg-gray-100 py-2 px-4 mt-6 mr-6"
+        >
           +
         </div>
         <input
@@ -70,8 +96,8 @@ function CourseCommentsBox(props) {
         />
         <button className="btn btn-blue h-12 rounded bg-blue-500 hover:bg-blue-700 text-white py-3 px-4 mt-6">
           Add Comment
-        </button>
-      </div>
+        </button> 
+      </div> : null }
     </div>
   );
 }
