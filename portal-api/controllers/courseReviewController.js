f78f3ae4ebdf11fd736c388e13a70561760ae588
vermaarn <arnavverma@mail.utoronto.ca>
2019-11-28 17:16:47 -0500

    added tempate controller for reit reviews n course

--- portal-api/controllers/courseReviewController.js
+++ portal-api/controllers/courseReviewController.js
@@ -1,1 +1,50 @@
 const Course = require("../models/courseModel");
+
+/**
+ * Course review Controller
+ *
+ * @description :: Server-side logic for managing course reviews.
+ */
+
+// (create) on signle object -- POST /
+function create(req, res) {}
+
+// (read) all objects -- GET /
+function list(req, res) {}
+
+// (read) one single object -- GET /:id
+function show(req, res) {
+  const id = req.params.id;
+
+  // Good practise: Validate id immediately.
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send(); // if invalid id, definitely can't find resource, 404.
+  }
+
+  // Otherwise, findById
+  CourseResource.findById(id)
+    .then(courseResource => {
+      if (!courseResource) {
+        res.status(404).send(); // could not find this student
+      } else {
+        res.send(courseResource);
+      }
+    })
+    .catch(error => {
+      res.status(500).send(); // server error
+    });
+}
+
+// (update) one single object PATH /:id
+function update(req, res) {}
+
+// (destroy) one single object DELETE /:id
+function destroy(req, res) {}
+
+module.exports = {
+  create,
+  list,
+  show,
+  update,
+  destroy
+};
--- portal-api/controllers/redditCommentController.js
+++ portal-api/controllers/redditCommentController.js
@@ -1,1 +1,18 @@
 const Course = require("../models/courseModel");
+
+/**
+ * Reddit Comment Controller
+ *
+ * @description :: Server-side logic for managing reddit reviews on a single course
+ */
+
+// (update) one single object PATH /:id
+function update(req, res) {}
+
+// (destroy) one single object DELETE /:id
+function destroy(req, res) {}
+
+module.exports = {
+  update,
+  destroy
+};
