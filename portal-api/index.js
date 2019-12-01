af24ad842f64bd78249cf66ca508fba6fbda7c31
vermaarn <arnavverma@mail.utoronto.ca>
2019-12-01 15:29:10 -0500

    using perm build folder

--- portal-api/index.js
+++ portal-api/index.js
@@ -34,7 +34,8 @@ const userRoutes = require("./routes/user");
 
 app.use(bodyParser.json());
 
-app.use(express.static("../frontend/build"));
+// console.log(path.join(__dirname, '../') + "frontend/build");
+app.use(express.static(path.join(__dirname, '../') + "frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, '../') + "frontend/build/index.html")
 });
