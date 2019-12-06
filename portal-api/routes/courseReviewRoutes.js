8b203e44f3886594ae040b2152bac7cbeab8ebc8
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2019-12-05 20:51:20 -0500

    wip: add course review permissions

--- portal-api/routes/courseReviewRoutes.js
+++ portal-api/routes/courseReviewRoutes.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const router = express.Router();
+
+const { isAdminUser, isUserAuthenticated } = require("../auth");
 const courseReviewController = require("../controllers/courseReviewController");
 
 /*
  * POST
  */
-router.post("/:school/:course/", courseReviewController.create);
+router.post(
+  "/:school/:course/",
+  [isUserAuthenticated],
+  courseReviewController.create
+);
 
 /*
  * DELETE
  */
-router.delete("/:school/:course/:id", courseReviewController.destroy);
+router.delete(
+  "/:school/:course/:id",
+  [isUserAuthenticated, isAdminUser],
+  courseReviewController.destroy
+);
 
 module.exports = router;
