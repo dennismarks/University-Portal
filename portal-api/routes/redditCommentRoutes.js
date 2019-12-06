4dd01b943a2ab707b8336b720dd7c093ef517a14
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2019-12-05 20:49:30 -0500

    wip: add permissions for reddit comment routes

--- portal-api/routes/redditCommentRoutes.js
+++ portal-api/routes/redditCommentRoutes.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const router = express.Router();
+
+const { isAdminUser, isUserAuthenticated } = require("../auth");
 const redditCommentController = require("../controllers/redditCommentController");
 
 /*
  * PATCH all reddit comments for the specfic course
  */
-router.patch("/:school/:course", redditCommentController.update);
+router.patch(
+  "/:school/:course",
+  [isUserAuthenticated],
+  redditCommentController.update
+);
 
 /*
  * DELETE
  */
-router.delete("/:id", redditCommentController.destroy);
+router.delete(
+  "/:id",
+  [isUserAuthenticated, isAdminUser],
+  redditCommentController.destroy
+);
 
 module.exports = router;
